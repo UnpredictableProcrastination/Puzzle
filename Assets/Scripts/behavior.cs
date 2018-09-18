63807288fa1eb88e20dff0c957c2ab54e16162d4
Master <RukiPomidory@yandex.ru>
2018-09-19 02:43:21 +0300

    can crush a Stalin like the Sovet Union

---
+++ Assets/Scripts/behavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class behavior : MonoBehaviour
+{
+    Vector3 dir = Vector3.zero;
+    bool pressed = false;
+    static Vector2 commonPosition;
+
+    void Update()
+    {
+        if(pressed)
+        {
+            Vector2 m_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //dir = (m_pos - (Vector2)transform.position).normalized;
+            commonPosition = m_pos;
+            //transform.Translate(dir * 0.05f);
+            transform.position = commonPosition;
+        }
+        transform.position = commonPosition;
+    }
+
+    private void OnMouseDown()
+    {
+        pressed = true;
+    }
+
+    private void OnMouseUp()
+    {
+        pressed = false;
+    }
+}
